91407158f94bb8e052bddb22046193b9e8e0af28
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-09-18 22:20:02 +0530

    Add database_url to configuration settings and update .env.example

--- Backend/app/core/config.py
+++ Backend/app/core/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     app_name: str = "NEXUS"
     app_env: str = "development"
+    database_url:str
 
     model_config = SettingsConfigDict(
         env_file=".env",
