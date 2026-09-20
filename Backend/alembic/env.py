edc1f75d13e8c480d67f081c838aede2120bdbf5
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-09-20 11:36:26 +0530

    Refactor Alembic environment to use SQLAlchemy engine from settings and update target metadata

--- Backend/alembic/env.py
+++ Backend/alembic/env.py
@@ -1,13 +1,18 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
+from sqlalchemy import create_engine
 from sqlalchemy import pool
 
 from alembic import context
+from app.core.config import settings
+from app.db.base import Base
+from app.models.user import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+ 
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -57,11 +62,10 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = create_engine(
+    settings.database_url,
+    poolclass=pool.NullPool,
+)
 
     with connectable.connect() as connection:
         context.configure(
