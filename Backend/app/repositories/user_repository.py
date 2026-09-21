4e17fca116a6fba47cdd556f68bb429103037e76
Muskan Chauhan <chauhanmuskan291980@gmail.com>
2026-09-21 12:05:40 +0530

    Refactor UserRepository methods for improved readability and consistency

--- Backend/app/repositories/user_repository.py
+++ Backend/app/repositories/user_repository.py
@@ -1,15 +1,28 @@
 from sqlalchemy import select
 from sqlalchemy.orm import Session
+
 from app.models.user import User
 
+
 class UserRepository:
+
     def get_by_email(
-            self,
-            db:Session,
-            email:str,
-    )->User|None:
-        statement = select(User).where(User.email==email)
+        self,
+        db: Session,
+        email: str,
+    ) -> User | None:
+        statement = select(User).where(User.email == email)
 
         return db.execute(statement).scalar_one_or_none()
 
+    def create(
+        self,
+        db: Session,
+        user: User,
+    ) -> User:
+        db.add(user)
+        db.commit()
+        db.refresh(user)
+
+        return user
 # scalar_one_or_none ----> One Matching user ---> Return that user , no matching user ---> Return Noen , Multiple matching users --- raise an error 
